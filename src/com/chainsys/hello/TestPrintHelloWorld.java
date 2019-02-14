fb25a81d97248477ee2f3e69f6a739b2b76d884f
mahe2111 <mahe2111@CSLH1707.global.chainsys.com>
2019-02-14 12:33:42 +0530

    new java class print hello world

---
+++ src/com/chainsys/hello/TestPrintHelloWorld.java
@@ -0,0 +1,10 @@
+package com.chainsys.hello;
+
+public class TestPrintHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
