f0f5d833ec8229e48ee9acda77074c482f237b8f
mahe2111 <mahe2111@CSLH1707.global.chainsys.com>
2019-02-14 12:36:57 +0530

    welcome message

---
+++ src/com/chainsys/hello/TestPrintWelcome.java
@@ -0,0 +1,10 @@
+package com.chainsys.hello;
+
+public class TestPrintWelcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome in Git. and code will be stored");
+	}	
+
+}
