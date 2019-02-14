045d884217cd9e088a8ac87646431555c7856f5a
mahe2111 <mahe2111@CSLH1707.global.chainsys.com>
2019-02-14 12:46:12 +0530

    update file

--- src/com/chainsys/hello/simple.java
+++ src/com/chainsys/hello/simple.java
@@ -1,7 +1,9 @@
+package com.chainsys.hello;
+
 class simple
 {
   public static void main(String args[])
   {
-    System.out.println("New File created in git hub")
+    System.out.println("New File created in git hub");
   }
 }
